f147d3b44cb5b5a001598860839cf01f5a3526c9
Brandon Day <brandontday7@gmail.com>
2017-09-09 19:15:10 +0000

    recieved a response from our request

--- https_example.js
+++ https_example.js
@@ -6,8 +6,9 @@ var options = {
   path: "/"
 };
 
-var callback = function () {
+var callback = function (response) {
   console.log("In response to handler callback");
+  console.log("Response: ", response);
 }
 
 console.log("I'm about to make a request");
