b0c15825308604fa12094029767c3793ac00099f
Brandon Day <brandontday7@gmail.com>
2017-09-09 19:21:21 +0000

    displays monty python script

--- https_example.js
+++ https_example.js
@@ -2,8 +2,8 @@ var https = require("https");
 console.log("I did it!");
 
 var options = {
-  host: "www.example.com",
-  path: "/"
+  host: "stream-large-file.herokuapp.com",
+  path: "/give-me-stuff-now"
 };
 
 var callback = function (response) {
