7ce7dbcd052d1ec54db0d723fc0dac449395f946
Brandon Day <brandontday7@gmail.com>
2017-09-09 19:10:52 +0000

    successfully made https request

--- https_example.js
+++ https_example.js
@@ -1,2 +1,18 @@
 var https = require("https");
 console.log("I did it!");
+
+var options = {
+  host: "www.example.com",
+  path: "/"
+};
+
+var callback = function () {
+  console.log("In response to handler callback");
+}
+
+console.log("I'm about to make a request");
+
+https.request(options, callback).end();
+
+console.log("I've made a request");
+
